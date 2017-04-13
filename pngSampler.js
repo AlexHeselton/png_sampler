ea32d7ba80819c27599ccd4309ba0e22d8c8ff3b
AlexHeselton <alex.heselton@gmail.com>
2017-04-13 13:52:58 -0700

    Commit for Taurin

--- pngSampler.js
+++ pngSampler.js
@@ -1,12 +1,6 @@
 //Bird sampler
-
-/////
-//To do
-// color on right bird - order of display changed fixed this!
-// fade properly - brain hurts too much 
-// function that places them in regards to screen width
-// size changes according to screen size 
-
+ 
+//variables to hold sound and images
 var one;
 var two;
 var three;
@@ -14,22 +8,24 @@ var img1;
 var img2;
 var img3;
 
+//arrays that pair the sound to each image
+var parrot = [];
+var hummingbird = [];
+var dove = [];
+
+//array that stores each sound and image pair
+var birdArray = [];
+
+//array that contains all the bird objects with image, sound, x and y positions, and size to be dislayed and activated
 var birdList = [];
-var sound =[];
-var img = [];
 
-// var parrot;
-// var hummingbird;
-// var dove;
-
+//the size of the birds (square)
 var birdWidth;
-var height; 
 
+//The number of birds to be displayed in the grid
 var birdNo = 16;
-var birdXPos;
-var birdYPos;
-var gridSize;
 
+//load the sound and images into the variables
 function preload() {
 	one = loadSound("https://cdn.rawgit.com/AlexHeselton/png_sampler/df50637e/one.wav");
 	two = loadSound("https://cdn.rawgit.com/AlexHeselton/png_sampler/df50637e/two.wav");
@@ -42,24 +38,32 @@ function preload() {
 function setup() {
 	createCanvas(windowWidth, windowHeight); 
 
-	sound = [one, two, three];
-	img = [img1, img2, img3];
-	print(img);
-	print(sound);
+	//define the sound and image pairs
+	parrot = [one, img1];
+	dove = [two, img2];
+	hummingbird = [three, img3];
 
+	//populate the birdArray with the pairs
+	birdArray = [parrot, dove, hummingbird];
+
+	//ERROR HERE IN LOGIC - this only finds the size if they were in a straight line, if there are 16 birds total,
+	//you should be dividing the screen by 4 not 16 as there should be four rows of four birds? this isn't the issue
+	// with them not displaying but it means they won't display right. 
+	
+	//define the size of the birds by the size of the screen and the number of birds to fit inside it
 	birdWidth = windowWidth / birdNo;
-	// parrot = new Bird(img1, one, 0, 0, birdWidth, birdWidth);
-	// dove = new Bird(img2, two, 400, 0, birdWidth, birdWidth);
-	// hummingbird = new Bird(img3, three, 800, 0, birdWidth, birdWidth);
-	// birdList = [parrot, dove, hummingbird];
-	// birdList[random(2)];
 
-	gridSize = 200;
-
+	//For loop to run for how many birds there are
 	for (var i = 0; i <= birdNo; i++) {
-		for (var x = gridSize; x <= windowWidth - gridSize; x += gridSize) {
-			for (var y = gridSize; y < windowHeight - gridSize; y += gridSize) {
-				birdList[i]= new Bird(img[Math.floor(Math.random() * img.length)], sound[Math.floor(Math.random() * sound.length)], x, y, birdWidth, birdWidth);
+		//create columns of birds, x value defined by the width
+		for (var x = 0; x < windowWidth; x += birdWidth) {
+			//y values defined by the height within each x value
+			for (var y = 0; y < windowHeight; y += birdWidth) {
+				//for each x y intersection draw a new random number
+				var thisBird = random(2);
+				//which then defines which sound and image pair is pushed into the bird object alongside the size and 
+				//position
+				birdList[i]= new Bird(birdArray[thisBird[0]], birdArray[thisBird[1]], x, y, birdWidth, birdWidth);
 			}
 		
 		print(birdList[i]);
@@ -73,13 +77,14 @@ function draw() {
 
 	background(255);
 
-
+	//In the draw loop iterate through the list of bird objects and activate their functions (display and play sound)
 	for (i = 0; i < birdNo; i++) {
 		birdList[i].display();
 		birdList[i].play();
 	}
 
 	print(birdWidth);
+
 	//print(birdWidth);
 
 	// parrot.display();
@@ -93,6 +98,7 @@ function draw() {
 
 }
 
+//bird object constructor function
 function Bird(tempImg, tempSound, tempX, tempY, tempW, tempH) {
 	this.img = tempImg;
 	this.sound = tempSound;
